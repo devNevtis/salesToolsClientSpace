21d8e38a073e404215a2c6688cbaf832f63dfba6
Diego <drbv27@gmail.com>
2025-02-11 11:08:18 -0500

    new loader for automatic login

---
+++ src/app/loadertest/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+    <div class="sk-cube-grid">
+      <div className="sk-cube sk-cube1"></div>
+      <div className="sk-cube sk-cube2"></div>
+      <div className="sk-cube sk-cube3"></div>
+      <div className="sk-cube sk-cube4"></div>
+      <div className="sk-cube sk-cube5"></div>
+      <div className="sk-cube sk-cube6"></div>
+      <div className="sk-cube sk-cube7"></div>
+      <div className="sk-cube sk-cube8"></div>
+      <div className="sk-cube sk-cube9"></div>
+    </div>
+    <p className="text-3xl text-teal-800 font-semibold">Verificando credenciales...</p>
+  </div>
+  )
+}
+
+export default page
